26428425eb19a15455b7dc23b41d02f3120d8ed4
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-06-25 15:26:07 +0800

    Added an UI and navigation bar

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Jirapat Kleepbua',
+        'title': 'Blog Post 1',
+        'content': 'I love Michael Lue and His FAT, too bad he isnt like Nick Young',
+        'date_posted': 'June 22, 2019'
+    },
+    {
+        'author': 'Ngimanita',
+        'title': 'Blog Post 2',
+        'content': 'I love P Toon!! and Fern my Side Chick',
+        'date_posted': 'June 23,2019'
+    },
+    {
+        'author': 'Moness',
+        'title': 'Blog Post 3',
+        'content': 'Michaeeeeeeeeel! Nine wants to go Hasong and Korean BBQ',
+        'date_posted': 'June 25, 2019'
+    }
+]
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
