16bf6d9366bb50b3758d3b852efd4465e49dbbb6
JamaicanFriedChicken <mikey_lue777@hotmail.com>
2019-07-02 16:34:56 +0800

    Added User validation

--- forms.py
+++ forms.py
@@ -13,6 +13,15 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
